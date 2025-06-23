606cf1b66881754edd79050ce5c7194e7c21a9d0
dortus86 <danyel.dsp@gmail.com>
2025-06-23 15:20:24 -0300

    iniciado o modal da section habilidades para dispositivos mobile

--- script.js
+++ script.js
@@ -31,27 +31,65 @@ btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
 
 //Navegaçao em Tab - Habilidades
-const tabMenu = document.querySelectorAll('.js-tabmenu li')
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function init_navegacaoTab() {
+    
+    const tabMenu = document.querySelectorAll('.js-tabmenu li')
+    const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if(tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo')
-    tabMenu[0].classList.add('selecao-ativo')
+    if(tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo')
+        tabMenu[0].classList.add('selecao-ativo')
 
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo')
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo')
+            })
+            tabMenu.forEach((icone) => {
+                icone.classList.remove('selecao-ativo')
+            })
+            tabMenu[index].classList.add('selecao-ativo')
+            tabContent[index].classList.add('ativo')
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', function() {
+                activeTab(index);
+            })
         })
-        tabMenu.forEach((icone) => {
-            icone.classList.remove('selecao-ativo')
-        })
-        tabMenu[index].classList.add('selecao-ativo')
-        tabContent[index].classList.add('ativo')
     }
+} init_navegacaoTab() 
 
-    tabMenu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', function() {
-            activeTab(index);
-        })
-    })
+
+const habilidadesModal = document.querySelectorAll(".js-tabmenu li");
+const habilidadesModalContent = document.querySelectorAll(".js-modal-habilidade");
+const botaoFecharModal = document.querySelectorAll(".js-modal-habilidades .js-modal-habilidade div button"
+);
+
+function ativarModal(index) {
+    habilidadesModalContent.forEach((modalContent) => {
+        modalContent.classList.remove('ativo')
+    });
+    habilidadesModal.forEach((modalIcone) => {
+        modalIcone.classList.remove('selecao-ativo')
+    });
+    habilidadesModal[index].classList.add("selecao-ativo");
+    habilidadesModalContent[index].classList.add("ativo");
+} 
+
+function fecharModal() {
+    habilidadesModalContent.forEach((modalContent) => {
+        modalContent.classList.remove("ativo");
+    });
 }
+
+habilidadesModal.forEach((itemModal, index) => {
+    itemModal.addEventListener('click', function() {
+        ativarModal(index);
+    })
+})
+
+botaoFecharModal.forEach((itemModal, index) => {
+    itemModal.addEventListener("click", function () {
+        fecharModal(index);
+    });
+})
